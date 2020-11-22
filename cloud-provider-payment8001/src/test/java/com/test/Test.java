ec53bdb93d956db26650eec1c870e901af435ffd
pranlolw <380566442@qq.com>
2020-11-22 09:56:27 +0800

    springcloud sleuth

---
+++ cloud-provider-payment8001/src/test/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate.getYear());
+    }
+}
