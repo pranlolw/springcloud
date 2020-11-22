33804f45da684124387a5dff07131776e500dddc
pranlolw <380566442@qq.com>
2020-11-22 09:33:23 +0800

    springcloud stream

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/st/springcloud/controller/SendMessageController.java
@@ -0,0 +1,19 @@
+package com.st.springcloud.controller;
+
+import com.st.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+
+}
